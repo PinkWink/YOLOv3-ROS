566e42c8578e8d860fe1996c82b0581b2576cd02
PinkWink <pinkwink.korea@gmail.com>
2019-11-24 17:53:12 +0900

    fix_cpu_version

--- yolov3_pytorch_ros/src/detector.py
+++ yolov3_pytorch_ros/src/detector.py
@@ -57,20 +57,20 @@ class DetectorManager():
         classes_name = rospy.get_param('~classes_name', 'coco.names')
         self.classes_path = os.path.join(package_path, 'classes', classes_name)
         self.gpu_id = rospy.get_param('~gpu_id', 0)
-        self.network_img_size = rospy.get_param('~img_size', 608)
+        self.network_img_size = rospy.get_param('~img_size', 416)
         self.publish_image = rospy.get_param('~publish_image')
-
+        
         # Initialize width and height
         self.h = 0
         self.w = 0
-
+        
         # Load net
         self.model = Darknet(self.config_path, img_size=self.network_img_size)
         self.model.load_weights(self.weights_path)
+
         if torch.cuda.is_available():
             self.model.cuda()
-        # else:
-        #     raise IOError('CUDA not found.')
+
         self.model.eval() # Set in evaluation mode
         rospy.loginfo("Deep neural network loaded")
 
@@ -80,7 +80,7 @@ class DetectorManager():
         # Load classes
         self.classes = load_classes(self.classes_path) # Extracts class labels from file
         self.classes_colors = {}
-
+        
         # Define subscribers
         self.image_sub = rospy.Subscriber(self.image_topic, Image, self.imageCb, queue_size = 1, buff_size = 2**24)
 
@@ -106,16 +106,17 @@ class DetectorManager():
 
         # Configure input
         input_img = self.imagePreProcessing(self.cv_image)
+
         if torch.cuda.is_available():
             input_img = Variable(input_img.type(torch.cuda.FloatTensor))
         else:
             input_img = Variable(input_img.type(torch.FloatTensor))
-
+        
         # Get detections from network
         with torch.no_grad():
             detections = self.model(input_img)
             detections = non_max_suppression(detections, 80, self.confidence_th, self.nms_th)
-
+        
         # Parse detections
         if detections[0] is not None:
             for detection in detections[0]:
@@ -149,27 +150,27 @@ class DetectorManager():
         if (self.publish_image):
             self.visualizeAndPublish(detection_results, self.cv_image)
         return True
-
+    
 
     def imagePreProcessing(self, img):
         # Extract image and shape
         img = np.copy(img)
         img = img.astype(float)
         height, width, channels = img.shape
-
+        
         if (height != self.h) or (width != self.w):
             self.h = height
             self.w = width
-
+            
             # Determine image to be used
             self.padded_image = np.zeros((max(self.h,self.w), max(self.h,self.w), channels)).astype(float)
-
+            
         # Add padding
         if (self.w > self.h):
             self.padded_image[(self.w-self.h)//2 : self.h + (self.w-self.h)//2, :, :] = img
         else:
             self.padded_image[:, (self.h-self.w)//2 : self.w + (self.h-self.w)//2, :] = img
-
+        
         # Resize and normalize
         input_img = resize(self.padded_image, (self.network_img_size, self.network_img_size, 3))/255.
 
@@ -196,35 +197,20 @@ class DetectorManager():
             x_p3 = output.bounding_boxes[index].xmax
             y_p3 = output.bounding_boxes[index].ymax
             confidence = output.bounding_boxes[index].probability
-            w = int(x_p3 - x_p1)
-            h = int(y_p3 - y_p1)
-            #if w > h:
-                #cv2.rectangle(imgOut, (int(x_p1)-20, int(y_p1)-20), (int(x_p3), int(y_p3)), (color[0],color[1],color[2]),2)
-
-
-            print(w, h)
 
             # Find class color
             if label in self.classes_colors.keys():
                 color = self.classes_colors[label]
             else:
                 # Generate a new color if first time seen this label
-                color = np.random.randint(0,188,3)
-                #color[0] = (255,255,255)
-                #color[1] = (198,198,198)
-                #color[2] = (27,27,27)
-
+                color = np.random.randint(0,198,3)
                 self.classes_colors[label] = color
-
+            
             # Create rectangle
-            cv2.rectangle(imgOut, (int(x_p1), int(y_p1)), (int(x_p3), int(y_p3)), (color[0],color[1],color[2]),2)
-            #print(color)
+            cv2.rectangle(imgOut, (int(x_p1), int(y_p1)), (int(x_p3), int(y_p3)), (color[0],color[1],color[2]),thickness)
             text = ('{:s}: {:.3f}').format(label,confidence)
-            cv2.putText(imgOut, text, (int(x_p1), int(y_p1-10)), font, fontScale, (0,0,0), thickness ,cv2.LINE_AA)
+            cv2.putText(imgOut, text, (int(x_p1), int(y_p1+20)), font, fontScale, (255,255,255), thickness ,cv2.LINE_AA)
 
-            # Create center
-            center = (int(((x_p1)+(x_p3))/2),int(((y_p1)+(y_p3))/2))
-            cv2.circle(imgOut,(center), 1, color, 3)
         # Publish visualization image
         image_msg = self.bridge.cv2_to_imgmsg(imgOut, "rgb8")
         self.pub_viz_.publish(image_msg)
@@ -236,3 +222,4 @@ if __name__=="__main__":
 
     # Define detector object
     dm = DetectorManager()
+
